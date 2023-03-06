65a5138a44ccc5e3e5a1563d8d1c7812ba2d41c5
Muhammad Rifyal Zukhrufil Makarim <zukhrufilrifyal@gmail.com>
2023-03-06 20:00:04 +0700

    onboard home

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:troli_gsc/app/routes/app_pages.dart';
 import 'package:troli_gsc/onboarding.dart';
 import 'app/widgets/loadingpage.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(GetMaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "App",
+    initialRoute: Routes.HOME,
+    getPages: AppPages.routes,
+    theme: ThemeData(fontFamily: 'Nunito'),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: Future.delayed(Duration(seconds: 5)),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return loadingpage();
-        } else {
-          return GetMaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: "Application",
-            home: onboarding(),
-          );
-        }
-      },
-    );
-  }
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return FutureBuilder(
+//       future: Future.delayed(Duration(seconds: 5)),
+//       builder: (context, snapshot) {
+//         if (snapshot.connectionState == ConnectionState.waiting) {
+//           return loadingpage();
+//         } else {
+//           return GetMaterialApp(
+//             debugShowCheckedModeBanner: false,
+//             title: "Application",
+//             home: onboarding(),
+//           );
+//         }
+//       },
+//     );
+//   }
+// }
